548d0f473b4fe17c2f9e93c195a9290e51ad295c
mustafabati <mustafa.bati9@gmail.com>
2025-01-08 22:01:41 +0300

    smtp configuration

--- DiscoverYourself/Resources/Views/AccumulationGoals/Index.tr.Designer.cs
+++ DiscoverYourself/Resources/Views/AccumulationGoals/Index.tr.Designer.cs
@@ -44,5 +44,95 @@ namespace DiscoverYourself.Resources.Views.AccumulationGoals {
                 resourceCulture = value;
             }
         }
+        
+        internal static string AccumulationGoalsMsg {
+            get {
+                return ResourceManager.GetString("AccumulationGoalsMsg", resourceCulture);
+            }
+        }
+        
+        internal static string DateMsg {
+            get {
+                return ResourceManager.GetString("DateMsg", resourceCulture);
+            }
+        }
+        
+        internal static string GoalsMsg {
+            get {
+                return ResourceManager.GetString("GoalsMsg", resourceCulture);
+            }
+        }
+        
+        internal static string TargetCurrencyMsg {
+            get {
+                return ResourceManager.GetString("TargetCurrencyMsg", resourceCulture);
+            }
+        }
+        
+        internal static string TargetGoldMsg {
+            get {
+                return ResourceManager.GetString("TargetGoldMsg", resourceCulture);
+            }
+        }
+        
+        internal static string TargetSilverMsg {
+            get {
+                return ResourceManager.GetString("TargetSilverMsg", resourceCulture);
+            }
+        }
+        
+        internal static string RealizedMsg {
+            get {
+                return ResourceManager.GetString("RealizedMsg", resourceCulture);
+            }
+        }
+        
+        internal static string RealizedCurrencyMsg {
+            get {
+                return ResourceManager.GetString("RealizedCurrencyMsg", resourceCulture);
+            }
+        }
+        
+        internal static string RealizedGoldMsg {
+            get {
+                return ResourceManager.GetString("RealizedGoldMsg", resourceCulture);
+            }
+        }
+        
+        internal static string RealizedSilverMsg {
+            get {
+                return ResourceManager.GetString("RealizedSilverMsg", resourceCulture);
+            }
+        }
+        
+        internal static string SaveMsg {
+            get {
+                return ResourceManager.GetString("SaveMsg", resourceCulture);
+            }
+        }
+        
+        internal static string UpdateChartMsg {
+            get {
+                return ResourceManager.GetString("UpdateChartMsg", resourceCulture);
+            }
+        }
+        
+        internal static string Currency {
+            get {
+                return ResourceManager.GetString("Currency", resourceCulture);
+            }
+        }
+        
+        internal static string GoldMsg {
+            get {
+                return ResourceManager.GetString("GoldMsg", resourceCulture);
+            }
+        }
+        
+        internal static string SilverMsg {
+            get {
+                return ResourceManager.GetString("SilverMsg", resourceCulture);
+            }
+        }
     }
 }
